123ac99dcb479bf40bd02d34b30abab48fe7b99f
lc <cun.liu@qq.com>
2016-11-22 19:53:43 +0800

    code format

--- dnsapi/dns_ali.sh
+++ dnsapi/dns_ali.sh
@@ -174,8 +174,7 @@ _clean() {
 
   local records="$(echo "$response" -n | _egrep_o "\"RecordId\":\"[^\"]*\"" | cut -d : -f 2 | tr -d \")"
   echo -n "$records" \
-  | while read -r record_id;
-  do
+  | while read -r record_id; do
     _delete_record_query "$record_id"
     _ali_rest "ignore"
   done
