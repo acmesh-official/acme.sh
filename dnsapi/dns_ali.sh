e23136694413656625b67045a39b3296b976bc6d
lc <cun.liu@qq.com>
2016-11-23 16:05:05 +0800

    fix bug. _ali_nonce not work on solaris

--- dnsapi/dns_ali.sh
+++ dnsapi/dns_ali.sh
@@ -105,7 +105,9 @@ _ali_urlencode() {
 }
 
 _ali_nonce() {
-  _head_n 1 </dev/urandom | _digest "sha256" hex | cut -c 1-31
+  #_head_n 1 </dev/urandom | _digest "sha256" hex | cut -c 1-31
+  #Not so good...
+  date +"%s%N"
 }
 
 _check_exist_query() {
