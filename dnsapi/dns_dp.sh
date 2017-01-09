bd48f91c4169df904be479211f03e68d202f3752
Bastian Bittorf <bittorf@bluebottle.com>
2017-01-09 13:43:15 +0100

    dnsapi/dns_dp.sh: shellcheck: fix 1 occurence of SC2126
    message:
    "Consider using grep -c instead of grep | wc"

--- dnsapi/dns_dp.sh
+++ dnsapi/dns_dp.sh
@@ -102,7 +102,7 @@ existing_records() {
   fi
 
   if _contains "$response" "Action completed successful"; then
-    count=$(printf "%s" "$response" | grep '<type>TXT</type>' | wc -l | tr -d ' ')
+    count=$(printf "%s" "$response" | grep -c '<type>TXT</type>' | tr -d ' ')
     record_id=$(printf "%s" "$response" | grep '^<id>' | tail -1 | cut -d '>' -f 2 | cut -d '<' -f 1)
     _debug record_id "$record_id"
     return 0
