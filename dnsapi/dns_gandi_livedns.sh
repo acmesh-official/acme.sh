839f18d052c0dff1d6e38c098087e68832788f39
neilpang <github@byneil.com>
2017-03-01 19:17:20 +0800

    fix format

--- dnsapi/dns_gandi_livedns.sh
+++ dnsapi/dns_gandi_livedns.sh
@@ -37,8 +37,8 @@ dns_gandi_livedns_add() {
   _debug sub_domain "$_sub_domain"
 
   _gandi_livedns_rest PUT "domains/$_domain/records/$_sub_domain/TXT" "{\"rrset_ttl\": 300, \"rrset_values\":[\"$txtvalue\"]}" \
-  && _contains "$response" '{"message": "Zone Record Created"}' \
-  && _info "Add $(__green "success")"
+    && _contains "$response" '{"message": "Zone Record Created"}' \
+    && _info "Add $(__green "success")"
 }
 
 #Usage: fulldomain txtvalue
