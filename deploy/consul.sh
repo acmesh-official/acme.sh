01076a278a4db1645852130489d16b60b0cbd38a
Rikard Gynnerstedt <rikard.gynnerstedt@gmail.com>
2017-06-26 13:50:52 +0200

    added double quotes as per travis checks

--- deploy/consul.sh
+++ deploy/consul.sh
@@ -39,25 +39,25 @@ consul_deploy() {
   
   # set base url for all uploads
   upload_base_url="${DEPLOY_CONSUL_URL}/${DEPLOY_CONSUL_ROOT_KEY}/${_cdomain}"
-  _debug upload_base_url $upload_base_url
+  _debug upload_base_url "$upload_base_url"
 
   # private
-  _info uploading $_ckey
+  _info uploading "$_ckey"
   response=$(_post "@${_ckey}" "${upload_base_url}/${_cdomain}.key" "" "PUT")
   _debug response "$response"
 
   # public
-  _info uploading $_ccert
+  _info uploading "$_ccert"
   response=$(_post "@${_ccert}" "${upload_base_url}/${_cdomain}.cer" "" "PUT")
   _debug response "$response"
 
   # ca
-  _info uploading $_cca
+  _info uploading "$_cca"
   response=$(_post "@${_cca}" "${upload_base_url}/ca.cer" "" "PUT")
   _debug response "$response"
 
   # fullchain
-  _info uploading $_cfullchain
+  _info uploading "$_cfullchain"
   response=$(_post "@${_cfullchain}" "${upload_base_url}/fullchain.cer" "" "PUT")
   _debug response "$response"
 
