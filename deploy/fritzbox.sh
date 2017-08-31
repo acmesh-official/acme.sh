6cb5377d73c2ad6a4c9e7adfc727b191f127910b
Manuel Friedli <manuel@fritteli.ch>
2017-08-31 17:25:08 +0200

    Fix bugs and more/new formatting errors.

--- deploy/fritzbox.sh
+++ deploy/fritzbox.sh
@@ -63,7 +63,7 @@ fritzbox_deploy() {
   _info "Log in to the FRITZ!Box"
   _fritzbox_challenge="$(wget --no-check-certificate -q -O - "${_fritzbox_url}/login_sid.lua" | sed -e 's/^.*<Challenge>//' -e 's/<\/Challenge>.*$//')"
   _fritzbox_hash="$(echo -n "${_fritzbox_challenge}-${_fritzbox_password}" | iconv -f ASCII -t UTF16LE | md5sum | awk '{print $1}')"
-  _fritzbox_sid="$(wget --no-check-certificate -q -O - "${_fritzbox_url}/login_sid.lua?sid=0000000000000000\&username=${_fritzbox_username}\&response=${_fritzbox_challenge}-${_fritzbox_hash}" | sed -e 's/^.*<SID>//' -e 's/<\/SID>.*$//')"
+  _fritzbox_sid="$(wget --no-check-certificate -q -O - "${_fritzbox_url}/login_sid.lua?sid=0000000000000000&username=${_fritzbox_username}&response=${_fritzbox_challenge}-${_fritzbox_hash}" | sed -e 's/^.*<SID>//' -e 's/<\/SID>.*$//')"
 
   if [ -z "${_fritzbox_sid}" ] || [ "${_fritzbox_sid}" = "0000000000000000" ]; then
     _err "Logging in to the FRITZ!Box failed. Please check username, password and URL."
@@ -74,22 +74,22 @@ fritzbox_deploy() {
   _post_request="$(_mktemp)"
   _post_boundary="---------------------------$(date +%Y%m%d%H%M%S)"
   {
-    printf -- "--%s\r\n" "${_post_boundary}";
-    printf "Content-Disposition: form-data; name=\"sid\"\r\n\r\n%s\r\n" "${_fritzbox_sid}";
-    printf -- "--%s\r\n""${_post_boundary}";
+    printf -- "--%s\r\n" "${_post_boundary}"
+    printf "Content-Disposition: form-data; name=\"sid\"\r\n\r\n%s\r\n" "${_fritzbox_sid}"
+    printf -- "--%s\r\n" "${_post_boundary}"
   } >>"${_post_request}"
   # _CERTPASSWORD_ is unset because Let's Encrypt certificates don't have a passwort. But if they ever do, here's the place to use it!
   _CERTPASSWORD_=
   {
-    printf "Content-Disposition: form-data; name=\"BoxCertPassword\"\r\n\r\n%s\r\n" "${_CERTPASSWORD_}";
-    printf -- "--%s\r\n" "${_post_boundary}";
-    printf "Content-Disposition: form-data; name=\"BoxCertImportFile\"; filename=\"BoxCert.pem\"\r\n";
-    printf "Content-Type: application/octet-stream\r\n\r\n";
+    printf "Content-Disposition: form-data; name=\"BoxCertPassword\"\r\n\r\n%s\r\n" "${_CERTPASSWORD_}"
+    printf -- "--%s\r\n" "${_post_boundary}"
+    printf "Content-Disposition: form-data; name=\"BoxCertImportFile\"; filename=\"BoxCert.pem\"\r\n"
+    printf "Content-Type: application/octet-stream\r\n\r\n"
   } >>"${_post_request}"
-  cat "${_ckey}${_cfullchain}" >>"${_post_request}"
+  cat "${_ckey}" "${_cfullchain}" >>"${_post_request}"
   {
-    printf "\r\n";
-    printf -- "--%s--" "${_post_boundary}";
+    printf "\r\n"
+    printf -- "--%s--" "${_post_boundary}"
   } >>"${_post_request}"
 
   _info "Upload certificate to the FRITZ!Box"
