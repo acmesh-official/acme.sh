52351d7dc8f0cccf3139e16ea56e5f1d001e6deb
martgras <martin@grasruck.net>
2018-03-13 12:43:07 +0100

    avoid side effects in _printargs
    A possible fix for https://github.com/Neilpang/acme.sh/issues/1356

--- acme.sh
+++ acme.sh
@@ -139,6 +139,7 @@ __red() {
 }
 
 _printargs() {
+  local _exitstatus="$?"
   if [ -z "$NO_TIMESTAMP" ] || [ "$NO_TIMESTAMP" = "0" ]; then
     printf -- "%s" "[$(date)] "
   fi
@@ -148,6 +149,8 @@ _printargs() {
     printf -- "%s" "$1='$2'"
   fi
   printf "\n"
+  # return the saved exit status 
+  return "$_exitstatus"
 }
 
 _dlg_versions() {
