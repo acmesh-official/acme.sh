4d2a0697edfbded749df960e0cbb8a770794bcfc
Felipe Braz <felipebraz@kinghost.com.br>
2018-03-26 10:49:34 -0300

    fix identation dnsapi/dns_kinghost.sh

--- dnsapi/dns_kinghost.sh
+++ dnsapi/dns_kinghost.sh
@@ -17,79 +17,79 @@ KING_Api="https://api.kinghost.net/acme"
 # Usage: add  _acme-challenge.www.domain.com   "XKrxpRBosdIKFzxW_CT3KLZNf6q0HG9i01zxXp5CPBs"
 # Used to add txt record
 dns_kinghost_add() {
-    fulldomain=$1
-    txtvalue=$2
+  fulldomain=$1
+  txtvalue=$2
 
-    KINGHOST_Username="${KINGHOST_Username:-$(_readaccountconf_mutable KINGHOST_Username)}"
-    KINGHOST_Password="${KINGHOST_Password:-$(_readaccountconf_mutable KINGHOST_Password)}"
-    if [ -z "$KINGHOST_Username" ] || [ -z "$KINGHOST_Password" ]; then
-        KINGHOST_Username=""
-        KINGHOST_Password=""
-        _err "You don't specify KingHost api password and email yet."
-        _err "Please create you key and try again."
-        return 1
-    fi
-    
-    #save the credentials to the account conf file.
-    _saveaccountconf_mutable KINGHOST_Username  "$KINGHOST_Username"
-    _saveaccountconf_mutable KINGHOST_Password  "$KINGHOST_Password"
+  KINGHOST_Username="${KINGHOST_Username:-$(_readaccountconf_mutable KINGHOST_Username)}"
+  KINGHOST_Password="${KINGHOST_Password:-$(_readaccountconf_mutable KINGHOST_Password)}"
+  if [ -z "$KINGHOST_Username" ] || [ -z "$KINGHOST_Password" ]; then
+    KINGHOST_Username=""
+    KINGHOST_Password=""
+    _err "You don't specify KingHost api password and email yet."
+    _err "Please create you key and try again."
+    return 1
+  fi
 
-    _debug "Getting txt records"
-    _kinghost_rest GET "dns" "name=$fulldomain&content=$txtvalue"
+  #save the credentials to the account conf file.
+  _saveaccountconf_mutable KINGHOST_Username  "$KINGHOST_Username"
+  _saveaccountconf_mutable KINGHOST_Password  "$KINGHOST_Password"
 
-    #This API call returns "status":"ok" if dns record does not exists
-    #We are creating a new txt record here, so we expect the "ok" status
-    if ! echo "$response" | grep '"status":"ok"' >/dev/null; then
-        _err "Error"
-        _err "$response"
-        return 1
-    fi
+  _debug "Getting txt records"
+  _kinghost_rest GET "dns" "name=$fulldomain&content=$txtvalue"
 
-    _kinghost_rest POST "dns" "name=$fulldomain&content=$txtvalue"
-    if ! echo "$response" | grep '"status":"ok"' >/dev/null; then
-        _err "Error"
-        _err "$response"
-        return 1
-    fi
+  #This API call returns "status":"ok" if dns record does not exists
+  #We are creating a new txt record here, so we expect the "ok" status
+  if ! echo "$response" | grep '"status":"ok"' >/dev/null; then
+    _err "Error"
+    _err "$response"
+    return 1
+  fi
 
-    return 0;
+  _kinghost_rest POST "dns" "name=$fulldomain&content=$txtvalue"
+  if ! echo "$response" | grep '"status":"ok"' >/dev/null; then
+    _err "Error"
+    _err "$response"
+    return 1
+  fi
+
+  return 0;
 }
 
 # Usage: fulldomain txtvalue
 # Used to remove the txt record after validation
 dns_kinghost_rm() {
-    fulldomain=$1
-    txtvalue=$2
+  fulldomain=$1
+  txtvalue=$2
 
-    KINGHOST_Password="${KINGHOST_Password:-$(_readaccountconf_mutable KINGHOST_Password)}"
-    KINGHOST_Username="${KINGHOST_Username:-$(_readaccountconf_mutable KINGHOST_Username)}"
-        if [ -z "$KINGHOST_Password" ] || [ -z "$KINGHOST_Username" ]; then
-        KINGHOST_Password=""
-        KINGHOST_Username=""
-        _err "You don't specify KingHost api key and email yet."
-        _err "Please create you key and try again."
-        return 1
-    fi
+  KINGHOST_Password="${KINGHOST_Password:-$(_readaccountconf_mutable KINGHOST_Password)}"
+  KINGHOST_Username="${KINGHOST_Username:-$(_readaccountconf_mutable KINGHOST_Username)}"
+    if [ -z "$KINGHOST_Password" ] || [ -z "$KINGHOST_Username" ]; then
+    KINGHOST_Password=""
+    KINGHOST_Username=""
+    _err "You don't specify KingHost api key and email yet."
+    _err "Please create you key and try again."
+    return 1
+  fi
 
-    _debug "Getting txt records"
-    _kinghost_rest GET "dns" "name=$fulldomain&content=$txtvalue"
+  _debug "Getting txt records"
+  _kinghost_rest GET "dns" "name=$fulldomain&content=$txtvalue"
 
-    #This API call returns "status":"ok" if dns record does not exists
-    #We are removing a txt record here, so the record must exists
-    if echo "$response" | grep '"status":"ok"' >/dev/null; then
-        _err "Error"
-        _err "$response"
-        return 1
-    fi
+  #This API call returns "status":"ok" if dns record does not exists
+  #We are removing a txt record here, so the record must exists
+  if echo "$response" | grep '"status":"ok"' >/dev/null; then
+    _err "Error"
+    _err "$response"
+    return 1
+  fi
 
-    _kinghost_rest DELETE "dns" "name=$fulldomain&content=$txtvalue"
-    if ! echo "$response" | grep '"status":"ok"' >/dev/null; then
-        _err "Error"
-        _err "$response"
-        return 1
-    fi
+  _kinghost_rest DELETE "dns" "name=$fulldomain&content=$txtvalue"
+  if ! echo "$response" | grep '"status":"ok"' >/dev/null; then
+    _err "Error"
+    _err "$response"
+    return 1
+  fi
 
-    return 0;
+  return 0;
 }
 
 ####################  Private functions below ##################################
