8eb76b4f99c869507f16d583064d77395062ec7c
Steven Miano <steven.miano@catalina.com>
2019-02-25 13:29:02 -0500

    formatting issues addressed

--- dnsapi/dns_ultra.sh
+++ dnsapi/dns_ultra.sh
@@ -8,25 +8,25 @@
 ULTRA_API="https://restapi.ultradns.com/v2/"
 
 #Usage: add _acme-challenge.www.domain.com "some_long_string_of_characters_go_here_from_lets_encrypt"
-dns_ultra_add(){
+dns_ultra_add() {
   fulldomain=$1
   txtvalue=$2
   export txtvalue
   ULTRA_USR="${ULTRA_USR:-$(_readaccountconf_mutable ULTRA_USR)}"
   ULTRA_PWD="${ULTRA_PWD:-$(_readaccountconf_mutable ULTRA_PWD)}"
   if [ -z "$ULTRA_USR" ] || [ -z "$ULTRA_PWD" ]; then
-  	ULTRA_USR=""
-  	ULTRA_PWD=""
-  	  _err "You didn't specify an UltraDNS username and password yet"
-  	  return 1
+    ULTRA_USR=""
+    ULTRA_PWD=""
+    _err "You didn't specify an UltraDNS username and password yet"
+    return 1
   fi
   # save the username and password to the account conf file.
   _saveaccountconf_mutable ULTRA_USR "$ULTRA_USR"
   _saveaccountconf_mutable ULTRA_PWD "$ULTRA_PWD"
   _debug "First detect the root zone"
   if ! _get_root "$fulldomain"; then
-  	_err "invalid domain"
-  	return 1
+    _err "invalid domain"
+    return 1
   fi
   _debug _domain_id "${_domain_id}"
   _debug _sub_domain "${_sub_domain}"
@@ -34,8 +34,8 @@ dns_ultra_add(){
   _debug "Getting txt records"
   _ultra_rest GET "zones/${_domain_id}/rrsets/TXT?q=value:${fulldomain}"
   if printf "%s" "$response" | grep \"totalCount\" >/dev/null; then
- 	_err "Error, it would appear that this record already exists. Please review existing TXT records for this domain."
-  	return 1
+    _err "Error, it would appear that this record already exists. Please review existing TXT records for this domain."
+    return 1
   fi
 
   _info "Adding record"
@@ -55,7 +55,7 @@ dns_ultra_add(){
 
 }
 
-dns_ultra_rm(){
+dns_ultra_rm() {
   fulldomain=$1
   txtvalue=$2
   export txtvalue
@@ -64,8 +64,8 @@ dns_ultra_rm(){
   if [ -z "$ULTRA_USR" ] || [ -z "$ULTRA_PWD" ]; then
     ULTRA_USR=""
     ULTRA_PWD=""
-      _err "You didn't specify an UltraDNS username and password yet"
-      return 1
+    _err "You didn't specify an UltraDNS username and password yet"
+    return 1
   fi
 
   _debug "First detect the root zone"
@@ -80,7 +80,7 @@ dns_ultra_rm(){
   _debug "Getting TXT records"
   _ultra_rest GET "zones/${_domain_id}/rrsets?q=kind:RECORDS+owner:${_sub_domain}"
 
-  if ! printf "%s" "$response" | grep \"resultInfo\" > /dev/null; then
+  if ! printf "%s" "$response" | grep \"resultInfo\" >/dev/null; then
     _err "There was an error in obtaining the resource records for ${_domain_id}"
     return 1
   fi
@@ -105,39 +105,39 @@ dns_ultra_rm(){
 # _sub_domain=_acme-challenge.www
 # _domain=domain.com
 # _domain_id=sdjkglgdfewsdfg
-_get_root(){
-	domain=$1
-	i=2
-	p=1
-	while true; do
-		h=$(printf "%s" "$domain" | cut -d . -f $i-100)
-		_debug h "$h"
+_get_root() {
+  domain=$1
+  i=2
+  p=1
+  while true; do
+    h=$(printf "%s" "$domain" | cut -d . -f $i-100)
+    _debug h "$h"
     _debug response "$response"
-		if [ -z "$h" ]; then
-			#not valid
-			return 1
-		fi
-		if ! _ultra_rest GET "zones"; then
-			return 1
-		fi
-		if _contains "${response}" "${h}." > /dev/null; then
-			_domain_id=$(echo "$response" | _egrep_o "${h}")
-			if [ "$_domain_id" ]; then
-				_sub_domain=$(printf "%s" "$domain" | cut -d . -f 1-$p)
-				_domain="${h}"
-        _debug sub_domain "${_sub_domain}"
-        _debug domain "${_domain}"
-				return 0
-			fi
-			return 1
-		fi
-		p=$i
-		i=$(_math "$i" + 1)
-	done
-	return 1
+  if [ -z "$h" ]; then
+    #not valid
+    return 1
+  fi
+  if ! _ultra_rest GET "zones"; then
+	  return 1
+  fi
+  if _contains "${response}" "${h}." > /dev/null; then
+    _domain_id=$(echo "$response" | _egrep_o "${h}")
+    if [ "$_domain_id" ]; then
+      _sub_domain=$(printf "%s" "$domain" | cut -d . -f 1-$p)
+      _domain="${h}"
+      _debug sub_domain "${_sub_domain}"
+      _debug domain "${_domain}"
+      return 0
+    fi
+      return 1
+    fi
+    p=$i
+    i=$(_math "$i" + 1)
+  done
+  return 1
 }
 
-_ultra_rest(){
+_ultra_rest() {
   m=$1
   ep="$2"
   data="$3"
@@ -149,17 +149,16 @@ _ultra_rest(){
   export _H2="Authorization: Bearer ${AUTH_TOKEN}"
 
   if [ "$m" != "GET" ]; then
-  	_debug data "${data}"
+    _debug data "${data}"
     response="$(_post "${data}" "${ULTRA_API}"/"${ep}" "" "${m}")"
   else
     response="$(_get "$ULTRA_API/$ep")"
   fi
 }
 
-_ultra_login(){
+_ultra_login() {
   AUTH_TOKEN=$(curl -X POST --data \
-  "grant_type=password&username=${ULTRA_USR}&password=${ULTRA_PWD}"\
-   "${ULTRA_API}authorization/token" | cut -d, -f3 | cut -d\" -f4)
+    "grant_type=password&username=${ULTRA_USR}&password=${ULTRA_PWD}" \
+    "${ULTRA_API}authorization/token" | cut -d, -f3 | cut -d\" -f4)
   export AUTH_TOKEN
 }
-
