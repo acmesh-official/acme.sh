ffa5472b31b69cedae6e29bc10edf689176d54a0
rserpent <53250916+rserpent@users.noreply.github.com>
2019-10-16 16:25:38 +0500

    fix whitespaces

--- dnsapi/dns_nic.sh
+++ dnsapi/dns_nic.sh
@@ -79,7 +79,7 @@ dns_nic_rm() {
     _err "Invalid domain"
     return 1
   fi
-  
+
   _debug _sub_domain "$_sub_domain"
   _debug _domain "$_domain"
   _debug _service "$_service"
@@ -129,28 +129,28 @@ _get_root() {
   p=1
 
   if ! _nic_rest GET "zones"; then
-  return 1
+    return 1
   fi
 
   _all_domains=$(printf "%s" "$response" | grep "idn-name" | sed -r "s/.*idn-name=\"(.*)\" name=.*/\1/g")
   _debug2 _all_domains "$_all_domains"
 
   while true; do
-   h=$(printf "%s" "$domain" | cut -d . -f "$i"-100)
-   _debug h "$h"
+    h=$(printf "%s" "$domain" | cut -d . -f "$i"-100)
+    _debug h "$h"
 
-   if [ -z "$h" ]; then
-     return 1
-   fi
+    if [ -z "$h" ]; then
+      return 1
+    fi
 
-   if _contains "$_all_domains" "^$h$"; then
-     _sub_domain=$(printf "%s" "$domain" | cut -d . -f 1-$p)
-     _domain=$h
-     _service=$(printf "%s" "$response" | grep "$_domain" | sed -r "s/.*service=\"(.*)\".*$/\1/")
-     return 0
-   fi
-   p="$i"
-   i=$(_math "$i" + 1)
+    if _contains "$_all_domains" "^$h$"; then
+      _sub_domain=$(printf "%s" "$domain" | cut -d . -f 1-$p)
+      _domain=$h
+      _service=$(printf "%s" "$response" | grep "$_domain" | sed -r "s/.*service=\"(.*)\".*$/\1/")
+      return 0
+    fi
+    p="$i"
+    i=$(_math "$i" + 1)
   done
   return 1
 }
@@ -165,20 +165,20 @@ _nic_rest() {
   export _H2="Authorization: Bearer $_auth_token"
 
   if [ "$m" != "GET" ]; then
-  _debug data "$data"
-  response=$(_post "$data" "$NIC_Api/dns-master/$ep" "" "$m")
+    _debug data "$data"
+    response=$(_post "$data" "$NIC_Api/dns-master/$ep" "" "$m")
   else
-  response=$(_get "$NIC_Api/dns-master/$ep")
+    response=$(_get "$NIC_Api/dns-master/$ep")
   fi
 
   if _contains "$response" "<errors>"; then
-  error=$(printf "%s" "$response" | grep "error code" | sed -r "s/.*<error code=.*>(.*)<\/error>/\1/g")
-  _err "Error: $error"
-  return 1
+    error=$(printf "%s" "$response" | grep "error code" | sed -r "s/.*<error code=.*>(.*)<\/error>/\1/g")
+    _err "Error: $error"
+    return 1
   fi
 
   if ! _contains "$response" "<status>success</status>"; then
-   return 1
+    return 1
   fi
   _debug2 response "$response"
   return 0
