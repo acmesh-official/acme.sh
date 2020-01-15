79ad0ff56baac1a338d4155e40f77bc8eeae8109
rewqazxv <41326691+rewqazxv@users.noreply.github.com>
2020-01-15 22:11:34 +0800

    Simplify code

--- acme.sh
+++ acme.sh
@@ -6362,18 +6362,11 @@ _checkSudo() {
       #it's root using sudo, no matter it's using sudo or not, just fine
       return 0
     fi
-    case "$SUDO_COMMAND" in
-      */su)
-        #it's a normal user doing `sudo su`, no problem
-        return 0
-        ;;
-    esac
-    for i in $(cat /etc/shells); do
-      if [ "$SUDO_COMMAND" = "$i" ]; then
-        #it's a normal user running `sudo -i` or `sudo -s`, fine
-        return 0
-      fi
-    done
+    if [ -n "$SUDO_COMMAND" ]; then
+      #it's a normal user doing "sudo su", or `sudo -i` or `sudo -s`
+      _endswith "$SUDO_COMMAND" /bin/su || grep "^$SUDO_COMMAND\$" /etc/shells >/dev/null 2>&1
+      return $?
+    fi
     #otherwise
     return 1
   fi
