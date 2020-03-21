f61f2d6e5e024e7e08687beaacf8a9807c3a6bee
ThiloGa <thilo.gass@gmail.com>
2020-03-21 19:28:16 +0100

    adaptations to the new api functions _get_root fully functional due to the extended api

--- dnsapi/dns_nm.sh
+++ dnsapi/dns_nm.sh
@@ -6,7 +6,7 @@
 # Environment variables:
 #
 #  - $NM_user      (your namemaster.de API username)
-#  - $NM_md5       (your namemaster.de API password_as_md5hash)
+#  - $NM_sha256       (your namemaster.de API password_as_sha256hash)
 #
 # Author: Thilo Gass <thilo.gass@gmail.com>
 # Git repo: https://github.com/ThiloGa/acme.sh
@@ -14,42 +14,53 @@
 #-- dns_nm_add() - Add TXT record --------------------------------------
 # Usage: dns_nm_add _acme-challenge.subdomain.domain.com "XyZ123..."
 
+namemaster_api="https://namemaster.de/api/api.php"
+
+
 dns_nm_add() {
   fulldomain=$1
   txt_value=$2
   _info "Using DNS-01 namemaster hook"
 
   NM_user="${NM_user:-$(_readaccountconf_mutable NM_user)}"
-  NM_md5="${NM_md5:-$(_readaccountconf_mutable NM_md5)}"
-  if [ -z "$NM_user" ] || [ -z "$NM_md5" ]; then
+  NM_sha256="${NM_sha256:-$(_readaccountconf_mutable NM_sha256)}"
+  if [ -z "$NM_user" ] || [ -z "$NM_sha256" ]; then
     NM_user=""
-    NM_md5=""
-    _err "No auth details provided. Please set user credentials using the \$NM_user and \$NM_md5 environment variables."
+    NM_sha256=""
+    _err "No auth details provided. Please set user credentials using the \$NM_user and \$NM_sha256 environment variables."
     return 1
   fi
-  #save the api user and md5 password to the account conf file.
+  #save the api user and sha256 password to the account conf file.
   _debug "Save user and hash"
   _saveaccountconf_mutable NM_user "$NM_user"
-  _saveaccountconf_mutable NM_md5 "$NM_md5"
+  _saveaccountconf_mutable NM_sha256 "$NM_sha256"
 
-  zone="$(echo "$fulldomain" | _egrep_o "[^.]+.[^.]+$")"
-  get="https://namemaster.de/api/api.php?User=$NM_user&Password=$NM_md5&Antwort=csv&Int=0&Typ=ACME&Zone=$zone&hostname=$fulldomain&TXT=$txt_value&Action=Auto&Lifetime=3600"
 
-  if ! erg="$(_get "$get")"; then
-    _err "error Deleting $zone TXT: $txt_value"
+  _debug "First detect the root zone"
+  if ! _get_root "$fulldomain"; then
+    _err "invalid domain" "$fulldomain"
+    return 1
+  fi
+
+  _info "die Zone lautet:" "$zone"
+
+  get="$namemaster_api?User=$NM_user&Password=$NM_sha256&Antwort=csv&Typ=ACME&zone=$zone&hostname=$fulldomain&TXT=$txt_value&Action=Auto&Lifetime=3600"
+
+  if !   erg="$(_get "$get")"
+  then
+    _err "error Adding $fulldomain TXT: $txt_value"
     return 1
   fi
 
   if _contains "$erg" "Success"; then
     _info "Success, TXT Added, OK"
   else
-    _err "error Adding $zone TXT: $txt_value erg: $erg"
+    _err "error Adding $fulldomain TXT: $txt_value erg: $erg"
     return 1
   fi
 
-  _debug "ok Auto $zone TXT: $txt_value erg: $erg"
+  _debug "ok Auto $fulldomain TXT: $txt_value erg: $erg"
   return 0
-
 }
 
 dns_nm_rm() {
@@ -58,30 +69,52 @@ dns_nm_rm() {
   txt_value=$2
 
   NM_user="${NM_user:-$(_readaccountconf_mutable NM_user)}"
-  NM_md5="${NM_md5:-$(_readaccountconf_mutable NM_md5)}"
-  if [ -z "$NM_user" ] || [ -z "$NM_md5" ]; then
+  NM_sha256="${NM_sha256:-$(_readaccountconf_mutable NM_sha256)}"
+  if [ -z "$NM_user" ] || [ -z "$NM_sha256" ]; then
     NM_user=""
-    NM_md5=""
-    _err "No auth details provided. Please set user credentials using the \$NM_user and \$NM_md5 environment variables."
+    NM_sha256=""
+    _err "No auth details provided. Please set user credentials using the \$NM_user and \$NM_sha256 environment variables."
     return 1
   fi
 
-  zone="$(echo "$fulldomain" | _egrep_o "[^.]+.[^.]+$")"
-  get="https://namemaster.de/api/api.php?User=$NM_user&Password=$NM_md5&Antwort=csv&Int=0&Typ=TXT&Zone=$zone&hostname=$fulldomain&TXT=$txt_value&Action=Delete_IN&TTL=0"
 
-  if ! erg="$(_get "$get")"; then
-    _err "error Deleting $zone TXT: $txt_value"
+  zone="$(echo "$fulldomain" | _egrep_o "[^.]+.[^.]+$")"
+  get="$namemaster_api?User=$NM_user&Password=$NM_sha256&Antwort=csv&Typ=TXT&Zone=$zone&hostname=$fulldomain&TXT=$txt_value&Action=Delete_IN"
+
+  if !   erg="$(_get "$get")"
+  then
+    _err "error Deleting $fulldomain TXT: $txt_value"
     return 1
   fi
 
   if _contains "$erg" "Success"; then
     _info "Success, TXT removed, OK"
   else
-    _err "error Auto $zone TXT: $txt_value erg: $erg"
+    _err "error Auto $fulldomain TXT: $txt_value erg: $erg"
     return 1
   fi
 
-  _debug "ok Auto $zone TXT: $txt_value erg: $erg"
+  _debug "ok Auto $fulldomain TXT: $txt_value erg: $erg"
   return 0
+
+}
+
+
+_get_root() {
+
+  domain=$1
+
+  get="$namemaster_api?User=$NM_user&Password=$NM_sha256&Typ=acme&hostname=$domain&Action=getzone&antwort=csv"
+
+  if ! zone="$(_get "$get")"
+  then
+    _err "error getting Zone"
+    return 1
+  else
+    if _contains "$zone" "hostname not found"
+    then
+      return 1
+    fi
+  fi
 
 }
