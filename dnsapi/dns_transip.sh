adfa1704e2188828562004e5fab05b1a731749c2
Maarten den Braber <m@mdbraber.com>
2020-05-19 16:38:23 +0200

    Update nonce calculation to use acme.sh methods instead of openssl command

--- dnsapi/dns_transip.sh
+++ dnsapi/dns_transip.sh
@@ -93,7 +93,7 @@ _transip_rest() {
 }
 
 _transip_get_token() {
-  nonce=$(openssl rand -hex 12)
+  nonce=$(echo "TRANSIP$(_time)" | _digest sha1 hex)
 
   data="{\"login\":\"${TRANSIP_Username}\",\"nonce\":\"${nonce}\",\"read_only\":\"${TRANSIP_Token_Read_Only}\",\"expiration_time\":\"${TRANSIP_Token_Expiration}\",\"label\":\"${TRANSIP_Token_Label}\",\"global_key\":\"${TRANSIP_Token_Global_Key}\"}"
   _debug data "$data"
