0eb27fac10967ff053360e9be134f9b21357601e
Yang Liu <liuyangc3@gmail.com>
2021-03-08 11:37:40 +0800

    Update dns_ali.sh

--- dnsapi/dns_ali.sh
+++ dnsapi/dns_ali.sh
@@ -5,7 +5,7 @@ Ali_API="https://alidns.aliyuncs.com/"
 #ALICLOUD_ACCESS_KEY="LTqIA87hOKdjevsf5"
 #ALICLOUD_SECRET_KEY="0p5EYueFNq501xnCPzKNbx6K51qPH2"
 
-#Usage:  dns_ali_add _acme-challenge.www.domain.com "XKrxpRBosdIKFzxW_CT3KLZNf6q0HG9i01zxXp5CPBs"
+#Usage: dns_ali_add _acme-challenge.www.domain.com "XKrxpRBosdIKFzxW_CT3KLZNf6q0HG9i01zxXp5CPBs"
 dns_ali_add() {
   fulldomain=$1
   txtvalue=$2
