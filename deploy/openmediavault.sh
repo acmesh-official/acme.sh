6bbf927f57f8a7b1dcdd1f24220c09397368a541
Ross Shen <sjtuross@msn.com>
2022-01-19 21:13:02 +0800

    omv deploy hook: separate DEPLOY_OMV_WEBUI_ADMIN and DEPLOY_OMV_SSH_USER

--- deploy/openmediavault.sh
+++ deploy/openmediavault.sh
@@ -23,25 +23,32 @@ openmediavault_deploy() {
     DEPLOY_OMV_HOST="$_cdomain"
   fi
 
-  _getdeployconf DEPLOY_OMV_USER
+  _getdeployconf DEPLOY_OMV_WEBUI_ADMIN
 
-  if [ -z "$DEPLOY_OMV_USER" ]; then
-    DEPLOY_OMV_USER="admin"
+  if [ -z "$DEPLOY_OMV_WEBUI_ADMIN" ]; then
+    DEPLOY_OMV_WEBUI_ADMIN="admin"
+  fi
+
+  _getdeployconf DEPLOY_OMV_SSH_USER
+
+  if [ -z "$DEPLOY_OMV_SSH_USER" ]; then
+    DEPLOY_OMV_SSH_USER="root"
   fi
 
   _savedeployconf DEPLOY_OMV_HOST "$DEPLOY_OMV_HOST"
-  _savedeployconf DEPLOY_OMV_USER "$DEPLOY_OMV_USER"
+  _savedeployconf DEPLOY_OMV_WEBUI_ADMIN "$DEPLOY_OMV_WEBUI_ADMIN"
+  _savedeployconf DEPLOY_OMV_SSH_USER "$DEPLOY_OMV_SSH_USER"
 
-  _command="omv-rpc -u $DEPLOY_OMV_USER 'CertificateMgmt' 'getList' '{\"start\": 0, \"limit\": -1}' | jq -r '.data[] | select(.name==\"/CN='$_cdomain'\") | .uuid'"
+  _command="omv-rpc -u $DEPLOY_OMV_WEBUI_ADMIN 'CertificateMgmt' 'getList' '{\"start\": 0, \"limit\": -1}' | jq -r '.data[] | select(.name==\"/CN='$_cdomain'\") | .uuid'"
   # shellcheck disable=SC2086
-  _uuid=$(ssh "root@$DEPLOY_OMV_HOST" "$_command")
+  _uuid=$(ssh "$DEPLOY_OMV_SSH_USER@$DEPLOY_OMV_HOST" "$_command")
   _debug _command "$_command"
 
   if [ -z "$_uuid" ]; then
     _info "[OMV deploy-hook] Domain $_cdomain has no certificate in openmediavault, creating it!"
-    _command="omv-rpc -u $DEPLOY_OMV_USER 'CertificateMgmt' 'create' '{\"cn\": \"test.example.com\", \"size\": 4096, \"days\": 3650, \"c\": \"\", \"st\": \"\", \"l\": \"\", \"o\": \"\", \"ou\": \"\", \"email\": \"\"}' | jq -r '.uuid'"
+    _command="omv-rpc -u $DEPLOY_OMV_WEBUI_ADMIN 'CertificateMgmt' 'create' '{\"cn\": \"test.example.com\", \"size\": 4096, \"days\": 3650, \"c\": \"\", \"st\": \"\", \"l\": \"\", \"o\": \"\", \"ou\": \"\", \"email\": \"\"}' | jq -r '.uuid'"
     # shellcheck disable=SC2086
-    _uuid=$(ssh "root@$DEPLOY_OMV_HOST" "$_command")
+    _uuid=$(ssh "$DEPLOY_OMV_SSH_USER@$DEPLOY_OMV_HOST" "$_command")
     _debug _command "$_command"
 
     if [ -z "$_uuid" ]; then
@@ -58,17 +65,17 @@ openmediavault_deploy() {
   _debug _key "$_key"
 
   _info "[OMV deploy-hook] Updating key and certificate in openmediavault"
-  _command="omv-rpc -u $DEPLOY_OMV_USER 'CertificateMgmt' 'set' '{\"uuid\":\"$_uuid\", \"certificate\":$_fullchain, \"privatekey\":$_key, \"comment\":\"acme.sh deployed $(date)\"}'"
+  _command="omv-rpc -u $DEPLOY_OMV_WEBUI_ADMIN 'CertificateMgmt' 'set' '{\"uuid\":\"$_uuid\", \"certificate\":$_fullchain, \"privatekey\":$_key, \"comment\":\"acme.sh deployed $(date)\"}'"
   # shellcheck disable=SC2029
-  _result=$(ssh "root@$DEPLOY_OMV_HOST" "$_command")
-  _debug _result "$_result"
+  _result=$(ssh "$DEPLOY_OMV_SSH_USER@$DEPLOY_OMV_HOST" "$_command")
 
   _debug _command "$_command"
+  _debug _result "$_result"
 
   _info "[OMV deploy-hook] Asking openmediavault to apply changes... (this could take some time, hang in there)"
-  _command="omv-rpc -u $DEPLOY_OMV_USER 'Config' 'applyChanges' '{\"modules\":[], \"force\": false}'"
+  _command="omv-rpc -u $DEPLOY_OMV_WEBUI_ADMIN 'Config' 'applyChanges' '{\"modules\":[], \"force\": false}'"
   # shellcheck disable=SC2029
-  _result=$(ssh "root@$DEPLOY_OMV_HOST" "$_command")
+  _result=$(ssh "$DEPLOY_OMV_SSH_USER@$DEPLOY_OMV_HOST" "$_command")
 
   _debug _command "$_command"
   _debug _result "$_result"
@@ -76,7 +83,7 @@ openmediavault_deploy() {
   _info "[OMV deploy-hook] Asking nginx to reload"
   _command="nginx -s reload"
   # shellcheck disable=SC2029
-  _result=$(ssh "root@$DEPLOY_OMV_HOST" "$_command")
+  _result=$(ssh "$DEPLOY_OMV_SSH_USER@$DEPLOY_OMV_HOST" "$_command")
 
   _debug _command "$_command"
   _debug _result "$_result"
