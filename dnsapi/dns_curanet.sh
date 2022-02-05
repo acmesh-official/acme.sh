dc61c9e277f4d34d34e141e4fe56afdb0e44fab6
peter <plh@tigermedia.dk>
2022-02-05 22:21:18 +0100

    description

--- dnsapi/dns_curanet.sh
+++ dnsapi/dns_curanet.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 
 #Script to use with curanet.dk, scannet.dk, wannafind.dk, dandomain.dk DNS management.
+#Requires api credentials with scope: dns
 #Author: Peter L. Hansen <peter@r12.dk>
 
 CURANET_REST_URL="https://api.curanet.dk/dns/v1/Domains"
