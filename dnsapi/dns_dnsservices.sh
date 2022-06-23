dc882e6279783efd8b6f3afa99093feff27fb56d
Bjarke Bruun <bbruun@gmail.com>
2022-06-23 08:06:28 +0200

    Removed empty space

--- dnsapi/dns_dnsservices.sh
+++ dnsapi/dns_dnsservices.sh
@@ -11,7 +11,7 @@ DNSServices_API=https://dns.services/api
 
 ########  Public functions #####################
 
-#Usage: dns_dnsservices_add   _acme-challenge.www.domain.com   "XKrxpRBosdIKFzxW_CT3KLZNf6q0HG9i01zxXp5CPBs"
+#Usage: dns_dnsservices_add  _acme-challenge.www.domain.com   "XKrxpRBosdIKFzxW_CT3KLZNf6q0HG9i01zxXp5CPBs"
 dns_dnsservices_add() {
   fulldomain=$1 
   txtvalue=$2
