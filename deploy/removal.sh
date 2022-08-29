b218e53bb2b2389cf4a725e4d592daad3f257d00
Stéphane Este-Gracias <sestegra@gmail.com>
2022-08-29 20:08:35 +0200

    Remove CSR (see #4274 discussion)
    Signed-off-by: Stéphane Este-Gracias <sestegra@gmail.com>

--- deploy/removal.sh
+++ deploy/removal.sh
@@ -6,24 +6,27 @@
 
 ########  Public functions #####################
 
-#domain keyfile certfile cafile fullchai
+#domain keyfile certfile cafile fullchain
 removal_deploy() {
   _cdomain="$1"
   _ckey="$2"
   _ccert="$3"
   _cca="$4"
   _cfullchain="$5"
+  _ccsr=${_ckey%.*}.csr
 
   _debug _cdomain "$_cdomain"
   _debug _ckey "$_ckey"
   _debug _ccert "$_ccert"
   _debug _cca "$_cca"
   _debug _cfullchain "$_cfullchain"
+  _debug _ccsr "$_ccsr"
 
   rm -f "$_ckey"
   rm -f "$_ccert"
   rm -f "$_cca"
   rm -f "$_cfullchain"
+  rm -f "$_ccsr"
 
   return 0
 }
