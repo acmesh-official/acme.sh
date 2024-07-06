51151293d7556dde713c197d0fdc83b97cbbe642
Владимир Алексеев <73811032+vlad-kms@users.noreply.github.com>
2024-07-06 21:01:25 +1000

    Remove `date -d` on macOS

--- dnsapi/dns_selectel.sh
+++ dnsapi/dns_selectel.sh
@@ -356,7 +356,7 @@ _get_auth_token() {
       _debug3 _login_name "$_login_name"
       _debug3 _login_id "$_login_id"
       _debug3 _project_name "$_project_name"
-      _debug3 _receipt_time "$(date -d @"$_receipt_time" -u)"
+      # _debug3 _receipt_time "$(date -d @"$_receipt_time" -u)"
       # check the validity of the token for the user and the project and its lifetime
       _dt_diff_minute=$((($(date +%s) - _receipt_time) / 60))
       _debug3 _dt_diff_minute "$_dt_diff_minute"
