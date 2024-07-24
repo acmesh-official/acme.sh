3cefcd8204167910ad05db860e428de4e5e34120
Joris van den Dries <joris.vandendries@amplexor.com>
2024-07-24 15:02:48 +0200

    change reference to static value

--- notify/teams.sh
+++ notify/teams.sh
@@ -46,7 +46,7 @@ teams_send() {
             \"contentType\": \"application/vnd.microsoft.card.adaptive\",
             \"contentUrl\": null,
             \"content\": {
-                \"$schema\": \"http://adaptivecards.io/schemas/adaptive-card.json\",
+                \"schema\": \"http://adaptivecards.io/schemas/adaptive-card.json\",
                 \"type\": \"AdaptiveCard\",
                 \"version\": \"1.2\",
                 \"body\": [
