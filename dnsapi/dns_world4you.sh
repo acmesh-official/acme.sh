157d3f9dc9b8afaec83ec0219dd9da1c9fbeab66
Lorenz Stechauner <lorenz.stechauner@necronda.net>
2024-11-30 17:25:19 +0100

    dns_word4you: Fix for OpenBSD

--- dnsapi/dns_world4you.sh
+++ dnsapi/dns_world4you.sh
@@ -218,7 +218,7 @@ _get_paketnr() {
   TLD="$domain"
   _debug domain "$domain"
   RECORD=$(echo "$fqdn" | cut -c"1-$((${#fqdn} - ${#TLD} - 1))")
-  PAKETNR=$(echo "$domains" | grep "$domain" | sed 's/^[^,]*, *\([0-9]*\).*$/\1/')
+  PAKETNR=$(echo "$domains" | grep -o " $domain.*" | sed 's/^[^,]*, *\([0-9]*\).*$/\1/')
   return 0
 }
 
