218934e76722697be3c258a7205daf8c1b5e26c0
asavin <alexis.savin@gmail.com>
2025-04-18 18:06:12 +0200

    Remove export ?

--- dnsapi/dns_efficientip.sh
+++ dnsapi/dns_efficientip.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-export dns_efficientip_info='efficientip.com
+dns_efficientip_info='efficientip.com
 Site: https://efficientip.com/
 Docs: github.com/acmesh-official/acme.sh/wiki/dnsapi2#dns_efficientip
 Options:
