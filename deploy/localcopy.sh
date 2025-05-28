06b9db9077cefae92ae3ad463c4df45740c7da05
invario <67800603+invario@users.noreply.github.com>
2025-05-28 12:28:09 -0400

    Update localcopy.sh
    Fix tabbing
    Add more debug/info
    Only save conf values if not blank and if successful

--- deploy/localcopy.sh
+++ deploy/localcopy.sh
@@ -21,82 +21,85 @@
 
 #domain keyfile certfile cafile fullchain
 localcopy_deploy() {
-  _cdomain="$1"
-  _ckey="$2"
-  _ccert="$3"
-  _cca="$4"
-  _cfullchain="$5"
+_cdomain="$1"
+_ckey="$2"
+_ccert="$3"
+_cca="$4"
+_cfullchain="$5"
 
-  _debug _cdomain "$_cdomain"
-  _debug _ckey "$_ckey"
-  _debug _ccert "$_ccert"
-  _debug _cca "$_cca"
-  _debug _cfullchain "$_cfullchain"
+_debug _cdomain "$_cdomain"
+_debug _ckey "$_ckey"
+_debug _ccert "$_ccert"
+_debug _cca "$_cca"
+_debug _cfullchain "$_cfullchain"
 
-  _getdeployconf DEPLOY_LOCALCOPY_CERTIFICATE
-  _getdeployconf DEPLOY_LOCALCOPY_CERTKEY
-  _getdeployconf DEPLOY_LOCALCOPY_FULLCHAIN
-  _getdeployconf DEPLOY_LOCALCOPY_CA
-  _getdeployconf DEPLOY_LOCALCOPY_RELOADCMD
+_getdeployconf DEPLOY_LOCALCOPY_CERTIFICATE
+_getdeployconf DEPLOY_LOCALCOPY_CERTKEY
+_getdeployconf DEPLOY_LOCALCOPY_FULLCHAIN
+_getdeployconf DEPLOY_LOCALCOPY_CA
+_getdeployconf DEPLOY_LOCALCOPY_RELOADCMD
 
-  if [ "$DEPLOY_LOCALCOPY_CERTIFICATE" ]; then
-        _info "Copying certificate"
-        _debug "Copying $_ccert to $DEPLOY_LOCALCOPY_CERTIFICATE"
-        eval "cp $_ccert $DEPLOY_LOCALCOPY_CERTIFICATE"
-        if [ $? -ne 0 ]; then
-                _err "Failed to copy certificate, aborting."
-                return 1;
-        fi;
+if [ "$DEPLOY_LOCALCOPY_CERTIFICATE" ]; then
+  _info "Copying certificate"
+  _debug "Copying $_ccert to $DEPLOY_LOCALCOPY_CERTIFICATE"
+  eval "cp $_ccert $DEPLOY_LOCALCOPY_CERTIFICATE"
+  if [ $? -ne 0 ]; then
+    _err "Failed to copy certificate, aborting."
+    return 1;
   fi;
-
-  if [ "$DEPLOY_LOCALCOPY_CERTKEY" ]; then
-        _info "Copying certificate key"
-        _debug "Copying $_ckey to $DEPLOY_LOCALCOPY_CERTKEY"
-        eval "cp $_ckey $DEPLOY_LOCALCOPY_CERTKEY"
-        if [ $? -ne 0 ]; then
-                _err "Failed to copy certificate key, aborting."
-                return 1;
-        fi;
-
-  fi;
-
-  if [ "$DEPLOY_LOCALCOPY_FULLCHAIN" ]; then
-        _info "Copying fullchain"
-        _debug "Copying $_cfullchain to $DEPLOY_LOCALCOPY_FULLCHAIN"
-        eval "cp $_cfullchain $DEPLOY_LOCALCOPY_FULLCHAIN"
-        if [ $? -ne 0 ]; then
-                _err "Failed to copy fullchain, aborting."
-                return 1;
-        fi;
-
-  fi;
-
-  if [ "$DEPLOY_LOCALCOPY_CA" ]; then
-        _info "Copying CA"
-        _debug "Copying $_cca to $DEPLOY_LOCALCOPY_CA"
-        eval "cp $_cca $DEPLOY_LOCALCOPY_CA"
-        if [ $? -ne 0 ]; then
-                _err "Failed to copy CA, aborting."
-                return 1;
-        fi;
-  fi;
-
-  _reload=$DEPLOY_LOCALCOPY_RELOADCMD
-  if eval $_reload; then
-        _info "Reload successful."
-  else
-        _err "Reload failed."
-  fi;
-
-# Save configuration
   _savedeployconf DEPLOY_LOCALCOPY_CERTIFICATE "$DEPLOY_LOCALCOPY_CERTIFICATE"
-  _savedeployconf DEPLOY_LOCALCOPY_CERTKEY "$DEPLOY_LOCALCOPY_CERTKEY"
-  _savedeployconf DEPLOY_LOCALCOPY_FULLCHAIN "$DEPLOY_LOCALCOPY_FULLCHAIN"
-  _savedeployconf DEPLOY_LOCALCOPY_CA "$DEPLOY_LOCALCOPY_CA"
-  _savedeployconf DEPLOY_LOCALCOPY_RELOADCMD "$DEPLOY_LOCALCOPY_RELOADCMD" "base64"
+fi;
 
-  _info "$(__green ""localcopy" deploy success")"
-  return 0
+if [ "$DEPLOY_LOCALCOPY_CERTKEY" ]; then
+  _info "Copying certificate key"
+  _debug "Copying $_ckey to $DEPLOY_LOCALCOPY_CERTKEY"
+  eval "cp $_ckey $DEPLOY_LOCALCOPY_CERTKEY"
+  if [ $? -ne 0 ]; then
+    _err "Failed to copy certificate key, aborting."
+    return 1;
+  fi;
+  _savedeployconf DEPLOY_LOCALCOPY_CERTKEY "$DEPLOY_LOCALCOPY_CERTKEY"
+fi;
+
+if [ "$DEPLOY_LOCALCOPY_FULLCHAIN" ]; then
+  _info "Copying fullchain"
+  _debug "Copying $_cfullchain to $DEPLOY_LOCALCOPY_FULLCHAIN"
+  eval "cp $_cfullchain $DEPLOY_LOCALCOPY_FULLCHAIN"
+  if [ $? -ne 0 ]; then
+    _err "Failed to copy fullchain, aborting."
+    return 1;
+  fi;
+  _savedeployconf DEPLOY_LOCALCOPY_FULLCHAIN "$DEPLOY_LOCALCOPY_FULLCHAIN"
+fi;
+
+if [ "$DEPLOY_LOCALCOPY_CA" ]; then
+  _info "Copying CA"
+  _debug "Copying $_cca to $DEPLOY_LOCALCOPY_CA"
+  eval "cp $_cca $DEPLOY_LOCALCOPY_CA"
+  if [ $? -ne 0 ]; then
+    _err "Failed to copy CA, aborting."
+    return 1;
+  fi;
+  _savedeployconf DEPLOY_LOCALCOPY_CA "$DEPLOY_LOCALCOPY_CA"
+fi;
+
+_reload=$DEPLOY_LOCALCOPY_RELOADCMD
+_debug "Running reloadcmd $_reload"
+
+if [ -z "$_reload" ]; then
+  _info "Reloadcmd not provided, skipping."
+else
+  _info "Reloading"
+  if eval $_reload; then
+    _info "Reload successful."
+    _savedeployconf DEPLOY_LOCALCOPY_RELOADCMD "$DEPLOY_LOCALCOPY_RELOADCMD" "base64"
+  else
+    _err "Reload failed."
+  fi;
+fi;
+
+_info "$(__green "'localcopy' deploy success")"
+return 0
 
 }
 
