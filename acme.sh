df350e6660df5b61e353a5daed7d7a1ee1f5ba4a
neil <gitpc@neilpang.com>
2025-09-15 19:34:54 +0200

    fix format

--- acme.sh
+++ acme.sh
@@ -437,7 +437,7 @@ _secure_debug3() {
 }
 
 __USE_TR_TAG=""
-if [ "$(echo "abc" | LANG=C tr a-z A-Z 2>/dev/null)" != "ABC" ] ; then
+if [ "$(echo "abc" | LANG=C tr a-z A-Z 2>/dev/null)" != "ABC" ]; then
   __USE_TR_TAG="1"
 fi
 export __USE_TR_TAG
@@ -446,7 +446,7 @@ _upper_case() {
   if [ "$__USE_TR_TAG" ]; then
     LANG=C tr '[:lower:]' '[:upper:]'
   else
-  # shellcheck disable=SC2018,SC2019
+    # shellcheck disable=SC2018,SC2019
     LANG=C tr '[a-z]' '[A-Z]'
   fi
 }
