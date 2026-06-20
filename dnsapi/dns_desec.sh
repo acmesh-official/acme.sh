b47c46a69f5e9a7a73d5dcfdd8d31f30abeb2b10
Alexander Stehlik <alex@stehlik.it>
2026-06-20 21:57:44 +0200

    fix(dns_desec): sleep after DNS record change to prevent rate limit issues
    Also: make sure the subname is lowercase to fix tests where
    the acmetestXyzRandomName subdomain is used.

--- dnsapi/dns_desec.sh
+++ dnsapi/dns_desec.sh
@@ -39,6 +39,7 @@ dns_desec_add() {
     _err "invalid domain"
     return 1
   fi
+  _sub_domain=$(echo "$_sub_domain" | _lower_case)
   _debug _sub_domain "$_sub_domain"
   _debug _domain "$_domain"
 
@@ -100,7 +101,7 @@ dns_desec_rm() {
     _err "invalid domain"
     return 1
   fi
-
+  _sub_domain=$(echo "$_sub_domain" | _lower_case)
   _debug _sub_domain "$_sub_domain"
   _debug _domain "$_domain"
 
@@ -150,6 +151,8 @@ _desec_rest() {
   if [ "$m" != "GET" ]; then
     _secure_debug2 data "$data"
     response="$(_post "$data" "$ep" "" "$m")"
+    _info "Sleeping 1s to respect deSEC write rate limit"
+    sleep 1
   else
     response="$(_get "$ep")"
   fi
