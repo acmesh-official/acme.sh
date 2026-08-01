d7f7ce3a6402b67e490e0c60e844d1105fd15d2a
Joel Samson <joel.samson@hotmail.com>
2026-07-31 22:02:48 -0400

    Refactor retry logic and improve error handling

--- dnsapi/dns_freemyip.sh
+++ dnsapi/dns_freemyip.sh
@@ -12,6 +12,7 @@ Author: Recolic Keghart <root@recolic.net>, @Giova96, ExtremeFiretop
 FREEMYIP_DNS_API="https://freemyip.com/update?"
 
 ################ Public functions ################
+
 #Usage: dns_freemyip_add    fulldomain    txtvalue
 dns_freemyip_add() {
   fulldomain="$1"
@@ -26,8 +27,10 @@ dns_freemyip_add() {
     _err "Please specify your token and try again."
     return 1
   fi
+  
   #save the credentials to the account conf file.
   _saveaccountconf_mutable FREEMYIP_Token "$FREEMYIP_Token"
+  
   if _is_root_domain_published "$fulldomain"; then
     _err "freemyip API don't allow you to set multiple TXT record for the same subdomain!"
     _err "You must apply certificate for only one domain at a time!"
@@ -36,6 +39,7 @@ dns_freemyip_add() {
     _debug "If you are testing this workflow in github pipeline or acmetest, please set TEST_DNS_NO_SUBDOMAIN=1 and TEST_DNS_NO_WILDCARD=1"
     return 1
   fi
+  
   # txtvalue must be url-encoded. But it's not necessary for acme txt value.
   _freemyip_get_until_ok "${FREEMYIP_DNS_API}token=$FREEMYIP_Token&domain=$fulldomain&txt=$txtvalue" 2>&1
   return $?
@@ -55,6 +59,7 @@ dns_freemyip_rm() {
     _err "Please specify your token and try again."
     return 1
   fi
+  
   #save the credentials to the account conf file.
   _saveaccountconf_mutable FREEMYIP_Token "$FREEMYIP_Token"
 
@@ -73,15 +78,21 @@ _get_root() {
 # There is random failure while calling freemyip API too fast. This function automatically retry until success.
 _freemyip_get_until_ok() {
   _fmi_url="$1"
-  i=1
-  while [ "$i" -le 8 ]; do
-    _debug "HTTP GET freemyip.com API '$_fmi_url', retry $i/8..."
+  _fmi_i=1
+
+  while [ "$_fmi_i" -le 8 ]; do
+    _debug "HTTP GET freemyip.com API '$_fmi_url', retry $_fmi_i/8..."
     _fmi_response="$(_get "$_fmi_url")"
     printf '%s\n' "$_fmi_response" >&2
-    printf '%s\n' "$_fmi_response" | grep OK && return 0
+
+    if _contains "$_fmi_response" "OK"; then
+      return 0
+    fi
+
     _sleep 1 # DO NOT send the request too fast
-    i=$((i + 1))
+    _fmi_i=$((_fmi_i + 1))
   done
+
   _err "Failed to request freemyip API. Server does not say 'OK'"
   return 1
 }
@@ -92,15 +103,19 @@ _is_root_domain_published() {
   _webroot="$(_get_root "$_fmi_d")"
 
   _info "Verifying '""$_fmi_d""' freemyip webroot (""$_webroot"") is not published yet"
-  i=1
-  while [ "$i" -le 3 ]; do
-    _debug "'$_webroot' ns lookup, retry $i/3..."
+  _fmi_i=1
+
+  while [ "$_fmi_i" -le 3 ]; do
+    _debug "'$_webroot' ns lookup, retry $_fmi_i/3..."
+
     if [ "$(_ns_lookup "$_fmi_d" TXT)" ]; then
       _debug "'$_webroot' already has a TXT record published!"
       return 0
     fi
+
     _sleep 10 # Give it some time to propagate the TXT record
-    i=$((i + 1))
+    _fmi_i=$((_fmi_i + 1))
   done
+
   return 1
 }
