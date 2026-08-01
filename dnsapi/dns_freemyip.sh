fc007a6a4e05de475b2944fb2b9c19f0ae8c7d45
Joel Samson <joel.samson@hotmail.com>
2026-07-31 21:52:20 -0400

    Fix error message for freemyip API request failure
    Remove existing token leak. Not my regression.

--- dnsapi/dns_freemyip.sh
+++ dnsapi/dns_freemyip.sh
@@ -82,7 +82,7 @@ _freemyip_get_until_ok() {
     _sleep 1 # DO NOT send the request too fast
     i=$((i + 1))
   done
-  _err "Failed to request freemyip API: $_fmi_url . Server does not say 'OK'"
+  _err "Failed to request freemyip API. Server does not say 'OK'"
   return 1
 }
 
