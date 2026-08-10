e79e07a783b46a3558095089ef950df32177ce83
XuChao <xuchao@guestin.cn>
2026-08-10 21:54:22 +0800

    1.fix config load 2.use _secre_debug2 to log password 3.use fullchain to deploy 4.fix hardcode id 5.change shebang

--- deploy/ikuai.sh
+++ deploy/ikuai.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env sh
 
 # Here is a script to deploy cert to ikuai using curl
 #
@@ -9,6 +9,7 @@
 # IKUAI_PORT="80"               - port , defaults to "80"
 # IKUAI_USERNAME="admin"        - username , defaults to "admin"
 # IKUAI_PASSWORD="yourPassword" - password
+# IKUAI_CERT_ID=1               - ikuai cert id ,default is 1, only 1 is support!!!
 #
 #returns 0 means success, otherwise error.
 #
@@ -28,11 +29,6 @@ ikuai_deploy() {
   _debug _cca "$_cca"
   _debug _cfullchain "$_cfullchain"
 
-  [ -n "$IKUAI_SCHEME" ] || IKUAI_SCHEME="http"
-  [ -n "$IKUAI_HOSTNAME" ] || IKUAI_HOSTNAME="192.168.9.1"
-  [ -n "$IKUAI_PORT" ] || IKUAI_PORT=80
-  [ -n "$IKUAI_USERNAME" ] || IKUAI_USERNAME="admin"
-
   # Get deploy conf
   _getdeployconf IKUAI_SCHEME
   _getdeployconf IKUAI_HOSTNAME
@@ -40,8 +36,15 @@ ikuai_deploy() {
   _getdeployconf IKUAI_USERNAME
   _getdeployconf IKUAI_PASSWORD
 
-  if [ -z "$IKUAI_HOSTNAME" ] || [ -z "$IKUAI_USERNAME" ] || [ -z "$IKUAI_PASSWORD" ]; then
-    _err "IKUAI_HOSTNAME ,IKUAI_USERNAME and IKUAI_PASSWORD is required ."
+  # Use default if not provided
+  [ -n "$IKUAI_SCHEME" ] || IKUAI_SCHEME="http"
+  [ -n "$IKUAI_HOSTNAME" ] || IKUAI_HOSTNAME="192.168.9.1"
+  [ -n "$IKUAI_PORT" ] || IKUAI_PORT=80
+  [ -n "$IKUAI_USERNAME" ] || IKUAI_USERNAME="admin"
+  [ -n "$IKUAI_CERT_ID" ] || IKUAI_CERT_ID=1
+
+  if [ -z "$IKUAI_PASSWORD" ]; then
+    _err "please define IKUAI_PASSWORD."
     return 1
   fi
 
@@ -55,36 +58,23 @@ ikuai_deploy() {
   _ikuai_url="$IKUAI_SCHEME://$IKUAI_HOSTNAME:$IKUAI_PORT"
   _pass_md5="$(printf "%s" "$IKUAI_PASSWORD" | _digest md5 hex | _lower_case)"
   _pass_salt="$(printf "salt_11%s" "$IKUAI_PASSWORD" | _base64)"
-  _login_req="{\"username\":\"$IKUAI_USERNAME\",\"passwd\":\"$_pass_md5\",\"pass\":\"$_pass_salt\",\"remember_password\":\"\"}"
-
   _debug2 _ikuai_url "$_ikuai_url"
 
+  _login_req="{\"username\":\"$IKUAI_USERNAME\",\"passwd\":\"$_pass_md5\",\"pass\":\"$_pass_salt\",\"remember_password\":\"\"}"
   _response=$(_post "$_login_req" "$_ikuai_url/Action/login" "" "POST" "application/json")
 
-  if ! _contains "$_response" "ErrMsg"; then
-    _err "Failed to login to ikuai : $_response"
-    return 1
-  fi
-  _err_msg="$(printf "%s" "$_response" | _normalizeJson | _egrep_o '"ErrMsg":"[^"]*"' | cut -d'"' -f4)"
+  _err_msg="$(printf "%s" "$_response" | _normalizeJson | _egrep_o '"ErrMsg":"[^"]*"' | cut -d'"' -f 4)"
   # check ErrMsg
   if [ "$_err_msg" != "Success" ]; then
     _err "Failed to login to ikuai: $_err_msg"
     return 1
   fi
   # check cookie
-  _cookie="$(grep -i '^set-cookie:' "$HTTP_HEADER" | _head_n 1 | cut -d " " -f 2)"
+  _cookie="$(grep -i '^set-cookie:' "$HTTP_HEADER" | _head_n 1 | cut -d " " -f 2 | sed 's/;.*//')"
   if [ -z "$_cookie" ]; then
     _err "Fail to get the cookie."
     return 1
   fi
-  _info "Login to ikuai success ,now save the config ... "
-
-  # Save the config
-  _savedeployconf IKUAI_SCHEME "$IKUAI_SCHEME"
-  _savedeployconf IKUAI_HOSTNAME "$IKUAI_HOSTNAME"
-  _savedeployconf IKUAI_PORT "$IKUAI_PORT"
-  _savedeployconf IKUAI_USERNAME "$IKUAI_USERNAME"
-  _savedeployconf IKUAI_PASSWORD "$IKUAI_PASSWORD"
 
   # Set cookie header
   _H1="Cookie: $_cookie username=$IKUAI_USERNAME; login=1"
@@ -92,26 +82,31 @@ ikuai_deploy() {
   _info "Deploy the cert to ikuai ... "
 
   # Should replace \n to @ ," " to #
-  _cert_content_single_line="$(<"$_ccert" tr '\n' '@' | tr ' ' '#')"
+  _cert_content_single_line="$(<"$_cfullchain" tr '\n' '@' | tr ' ' '#')"
   _key_content_single_line="$(<"$_ckey" tr '\n' '@' | tr ' ' '#')"
 
   _debug2 _cert_content_single_line "$_cert_content_single_line"
-  _debug2 _key_content_single_line "$_key_content_single_line"
+  _secure_debug2 _key_content_single_line "$_key_content_single_line"
 
-  _key_manager_req="{\"func_name\":\"key_manager\",\"action\":\"save\",\"param\":{\"ca\":\"$_cert_content_single_line\",\"key\":\"$_key_content_single_line\",\"id\":1,\"enabled\":\"yes\",\"comment\":\"\"}}"
+  _key_manager_req="{\"func_name\":\"key_manager\",\"action\":\"save\",\"param\":{\"ca\":\"$_cert_content_single_line\",\"key\":\"$_key_content_single_line\",\"id\":$IKUAI_CERT_ID,\"enabled\":\"yes\",\"comment\":\"\"}}"
   _response=$(_post "$_key_manager_req" "$_ikuai_url/Action/call" "" "POST" "application/json")
 
-  _err_msg="$(printf "%s" "$_response" | _normalizeJson | _egrep_o '"ErrMsg":"[^"]*"' | cut -d'"' -f4)"
-
-  if ! _contains "$_response" "ErrMsg"; then
-    _err "Failed to save cert to ikuai : $_response"
-    return 1
-  fi
+  _err_msg="$(printf "%s" "$_response" | _normalizeJson | _egrep_o '"ErrMsg":"[^"]*"' | cut -d'"' -f 4)"
   # check ErrMsg
   if [ "$_err_msg" != "Success" ]; then
     _err "Failed to save cert to ikuai: $_err_msg"
     return 1
   fi
+
+  _info "Save the deploy config ... "
+  # Save the config
+  _savedeployconf IKUAI_SCHEME "$IKUAI_SCHEME"
+  _savedeployconf IKUAI_HOSTNAME "$IKUAI_HOSTNAME"
+  _savedeployconf IKUAI_PORT "$IKUAI_PORT"
+  _savedeployconf IKUAI_USERNAME "$IKUAI_USERNAME"
+  _savedeployconf IKUAI_PASSWORD "$IKUAI_PASSWORD"
+  _savedeployconf IKUAI_CERT_ID "$IKUAI_CERT_ID"
+
   _info "Deploy the cert to ikuai success ,now enjoy it :>! "
 
   return 0
