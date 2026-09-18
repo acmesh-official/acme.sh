70b1336151585110083f8e3ea2808d03fc020dde
Eyal Kalderon <ebkalderon@gmail.com>
2026-09-17 20:20:44 -0400

    Improve log output and inline docs some more

--- deploy/redfish.sh
+++ deploy/redfish.sh
@@ -59,76 +59,80 @@ redfish_deploy() {
   _getdeployconf DEPLOY_REDFISH_USE_BASIC_AUTH
   _getdeployconf DEPLOY_REDFISH_RESTART_BMC
 
-  # 1. Authenticate with the Redfish server and store the auth header.
-  #    * Create new Redfish session token using the username/password provided.
-  #    * Unless we have `DEPLOY_REDFISH_USE_BASIC_AUTH` defined, where we will
-  #      authenticate via `Authorization: Basic $(_base64 "username:password")`.
-
   # Scoped to this hook's own subshell -- does not affect the rest of the
   # acme.sh run (e.g. the connection to the ACME CA).
   export HTTPS_INSECURE=1
 
+  # 1. Authenticate with the Redfish server and store the auth header.
+
   _redfish_authenticate "$DEPLOY_REDFISH_HOST" "$DEPLOY_REDFISH_USERNAME" "$DEPLOY_REDFISH_PASSWORD" "$DEPLOY_REDFISH_USE_BASIC_AUTH"
   trap '_redfish_end_session "$DEPLOY_REDFISH_HOST" "${_redfish_session:-}"' EXIT INT
 
-  # 2. Verify Redfish server supports certificate management.
+  # 2. Verify Redfish server supports certificate management API.
 
   _redfish_response="$(_get "https://${_redfish_host}/redfish/v1/")"
   _redfish_managers_path="$(echo "${_redfish_response}" | jq -r '.Managers.["@odata.id"]')"
   _redfish_certificate_service_path="$(echo "${_redfish_response}" | jq -r '.CertificateService.["@odata.id"]')"
 
   if _contains "${_redfish_certificate_service_path}" 'null'; then
-    _err "Redfish server doesn't support certificate management API."
+    _err "Redfish server ${_redfish_host} doesn't support certificate management API."
     return 1
   fi
 
   # 3. Verify supported key algorithms/lengths/curves and that key is compatible.
 
-  if _isRSA "${_ckey}"; then
-    _redfish_key_algo='RSA'
-  elif _isEcc "${_ckey}"; then
-    _redfish_key_algo='ECDSA'
+  if [ -n "${_ckey}" ]; then
+    _debug 'Identifying compatibility of private key with Redfish server.'
+
+    if _isRSA "${_ckey}"; then
+      _redfish_key_algo='RSA'
+    elif _isEcc "${_ckey}"; then
+      _redfish_key_algo='ECDSA'
+    else
+      _err 'Private key uses unknown cryptographic algorithm!'
+      return 1
+    fi
+
+    _redfish_response="$(_get "https://${_redfish_host}${_redfish_certificate_service_path}")"
+    _redfish_generate_csr_info_path="$(echo "${_redfish_response}" | jq -r '.Actions["#CertificateService.GenerateCSR"].["@Redfish.ActionInfo"]')"
+    _redfish_response="$(_get "https://${_redfish_host}${_redfish_generate_csr_info_path}")"
+    _redfish_allowed_key_algos="$(echo "${_redfish_response}" | jq -r '.Parameters[] | select(.Name == "KeyPairAlgorithm") | .AllowableValues[]' | paste -sd ', ' -)"
+
+    if [ -z "${_redfish_allowed_key_algos}" ]; then
+      _redfish_allowed_key_algos='TCG_ALG_RSA'
+    fi
+
+    case "${_redfish_key_algo}:${_redfish_allowed_key_algos}" in
+    RSA:*RSA*)
+      _redfish_allowed_key_bit_lengths="$(echo "${_redfish_response}" | jq -r '.Parameters[] | select(.Name == "KeyBitLength")')"
+      _redfish_key_bits_min="$(echo "${_redfish_allowed_key_bit_lengths}" | jq -r '.MinimumValue')"
+      _redfish_key_bits_max="$(echo "${_redfish_allowed_key_bit_lengths}" | jq -r '.MaximumValue')"
+
+      # shellcheck disable=SC2154 # Le_Keylength is set by acme.sh core, not this hook
+      if [ "${Le_Keylength}" -le "${_redfish_key_bits_min}" ] || [ "${Le_Keylength}" -gt "${_redfish_key_bits_max}" ]; then
+        _err "Unsupported RSA private key length ${Le_Keylength}!"
+        _err "Please re-run \`acme.sh\` with \`--keylength\` set to a value between ${_redfish_allowed_key_bit_length_max} and ${_redfish_allowed_key_bit_length_min}."
+        return 1
+      fi
+      ;;
+    ECDSA:*ECDSA*)
+      _redfish_allowed_key_curve_ids="$(echo "${_redfish_response}" | jq -r '.Parameters[] | select(.Name == "KeyCurveId") | .AllowableValues[]')"
+
+      # shellcheck disable=SC2154 # Le_Keylength is set by acme.sh core, not this hook
+      if ! _contains "${_redfish_allowed_key_curve_ids}" "${Le_Keylength}"; then
+        _err "Unsupported ECDSA private key type! Supports only: ${_redfish_allowed_key_curve_ids}"
+        return 1
+      fi
+      ;;
+    *)
+      _err "This Redfish server does not support ${_redfish_key_algo} private keys! Supports only: ${_redfish_allowed_key_algos}"
+      return 1
+      ;;
+    esac
+
   else
-    _err 'Unknown cryptographic algorithm!'
-    return 1
+    _info "Not checking cipher suite compatibility with ${_redfish_host}, due to \`--sign-csr\`. Assuming correct private key is already on the server."
   fi
-
-  _redfish_response="$(_get "https://${_redfish_host}${_redfish_certificate_service_path}")"
-  _redfish_generate_csr_info_path="$(echo "${_redfish_response}" | jq -r '.Actions["#CertificateService.GenerateCSR"].["@Redfish.ActionInfo"]')"
-  _redfish_response="$(_get "https://${_redfish_host}${_redfish_generate_csr_info_path}")"
-  _redfish_allowed_key_algos="$(echo "${_redfish_response}" | jq -r '.Parameters[] | select(.Name == "KeyPairAlgorithm") | .AllowableValues[]')"
-
-  if [ -z "${_redfish_allowed_key_algos}" ]; then
-    _redfish_allowed_key_algos='TCG_ALG_RSA'
-  fi
-
-  case "${_redfish_key_algo}:${_redfish_allowed_key_algos}" in
-  RSA:*RSA*)
-    _redfish_allowed_key_bit_lengths="$(echo "${_redfish_response}" | jq -r '.Parameters[] | select(.Name == "KeyBitLength")')"
-    _redfish_key_bits_min="$(echo "${_redfish_allowed_key_bit_lengths}" | jq -r '.MinimumValue')"
-    _redfish_key_bits_max="$(echo "${_redfish_allowed_key_bit_lengths}" | jq -r '.MaximumValue')"
-
-    # shellcheck disable=SC2154 # Le_Keylength is set by acme.sh core, not this hook
-    if [ "${Le_Keylength}" -le "${_redfish_key_bits_min}" ] || [ "${Le_Keylength}" -gt "${_redfish_key_bits_max}" ]; then
-      _err "Unsupported RSA private key length ${Le_Keylength}!"
-      _err "Please re-run \`acme.sh\` with \`--keylength\` set to a value between ${_redfish_allowed_key_bit_length_max} and ${_redfish_allowed_key_bit_length_min}."
-      return 1
-    fi
-    ;;
-  ECDSA:*ECDSA*)
-    _redfish_allowed_key_curve_ids="$(echo "${_redfish_response}" | jq -r '.Parameters[] | select(.Name == "KeyCurveId") | .AllowableValues[]')"
-
-    # shellcheck disable=SC2154 # Le_Keylength is set by acme.sh core, not this hook
-    if ! _contains "${_redfish_allowed_key_curve_ids}" "${Le_Keylength}"; then
-      _err "Unsupported ECDSA private key type! Supports only: ${_redfish_allowed_key_curve_ids}"
-      return 1
-    fi
-    ;;
-  *)
-    _err "This Redfish server does not support ${_redfish_key_algo} private keys! Supports only: ${_redfish_allowed_key_algos}"
-    return 1
-    ;;
-  esac
 
   # 4. Perform 3.3 of this PDF: https://www.dmtf.org/sites/default/files/standards/documents/DSP2059_1.2.0.pdf
   #    * When deploying, do it according to 2.2.1.1 "Web Service Certificates".
@@ -144,7 +148,7 @@ redfish_deploy() {
 
   _getdeployconf DEPLOY_REDFISH_MANAGER
 
-  if [ -z "${DEPLOY_REDFISH_MANAGER}" ] && [ "${_redfish_num_managers}" != "1" ]; then
+  if [ -z "${DEPLOY_REDFISH_MANAGER}" ] && [ "${_redfish_num_managers}" != '1' ]; then
     if [ "${_redfish_num_managers}" = "0" ]; then
       _err "Unable to identify any Redfish managers."
     else
@@ -161,7 +165,7 @@ redfish_deploy() {
   _redfish_response="$(_get "https://${_redfish_host}${_redfish_network_protocol_path}/HTTPS/Certificates")"
   _redfish_num_certificates="$(echo "${_redfish_response}" | jq -r '.["Members@odata.count"]')"
 
-  if [ "${_redfish_num_certificates}" != "1" ]; then
+  if [ "${_redfish_num_certificates}" != '1' ]; then
     _redfish_all_managers="$(echo "${_redfish_response}" | jq -c '[.Members[].["@odata.id"]]')"
     _err "Multiple web service HTTPS certificates identified (${_redfish_all_managers}), but expected exactly one."
     return 1
@@ -174,23 +178,29 @@ redfish_deploy() {
     _redfish_ckey_pkcs8="$(_mktemp)"
 
     if ! _toPkcs8 "${_redfish_ckey_pkcs8}" "${_ckey}"; then
-      _err 'Failed to convert private key to unencrypted PKCS#8 format!'
+      _err 'Failed to convert private key to PKCS#8 format!'
       return 1
     fi
 
-    _redfish_certificate_str="$(paste -sd '\n' "${_ckey}" "${_cfullchain}" | _json_encode)"
+    _info "Uploading private key and full certificate chain to Redfish server."
+    _redfish_certificate_str="$(paste -sd '\n' "${_redfish_ckey_pkcs8}" "${_cfullchain}" | _json_encode)"
     _redfish_certificate_type="PEMchain"
   else
+    _info "Uploading only certificate chain to Redfish server, due to \`--sign-csr\`."
     _redfish_certificate_str="$(_json_encode <"${_cfullchain}")"
     _redfish_certificate_type="PEM"
   fi
+
+  _debug _redfish_certificate_path "${_redfish_certificate_path}"
+  _debug _redfish_certificate_type "${_redfish_certificate_type}"
+  _secure_debug _redfish_certificate_str "${_redfish_certificate_str}"
 
   _redfish_body="$(printf '{"CertificateString":"%s","CertificateType":"%s","CertificateUri":{"@odata.id":"%s"}}' "${_redfish_certificate_str}" "${_redfish_certificate_type}" "${_redfish_certificate_path}")"
   _redfish_response="$(_post "${_redfish_body}" "https://${_redfish_host}${_redfish_certificate_service_path}/Actions/CertificateService.ReplaceCertificate" '' 'POST' 'application/json')"
   _code="$(_egrep_o <"${HTTP_HEADER}" '^HTTP[^ ]* [0-9]+' | _tail_n 1 | tr -d '\r\n' | cut -d ' ' -f 2)"
 
-  if [ "${_code}" != "204" ]; then
-    _err "Failed to upload certificate chain and private key! Status code: ${_code}"
+  if [ "${_code}" != '204' ]; then
+    _err "Failed to update Redfish server TLS certificate! Status code: ${_code}"
     _err "Response: ${_redfish_response}"
     return 1
   fi
@@ -198,13 +208,13 @@ redfish_deploy() {
   _info 'Successfully updated Redfish server TLS certificate!'
 
   if [ -n "${DEPLOY_REDFISH_RESTART_BMC}" ]; then
-    _info 'Attempting to restart BMC gracefully...'
+    _info 'Attempting to restart BMC gracefully.'
 
     _redfish_response="$(_post '{"ResetType":"GracefulRestart"}' "https://${_redfish_host}${_redfish_manager_path}/Actions/Manager.Reset" '' 'POST' 'application/json')"
     _code="$(_egrep_o <"${HTTP_HEADER}" '^HTTP[^ ]* [0-9]+' | _tail_n 1 | tr -d '\r\n' | cut -d ' ' -f 2)"
 
-    if [ "${_code}" = "204" ]; then
-      _info "Successfully sent graceful restart command to BMC. Once complete, the new TLS certificate will be active."
+    if [ "${_code}" = '204' ]; then
+      _info "Successfully sent graceful restart command to BMC. After restarting, the new TLS certificate will be active."
     else
       _err "Failed to gracefully restart BMC! Status code: ${_code}"
       _err "Response: ${_redfish_response}"
@@ -226,13 +236,15 @@ _redfish_authenticate() {
   export _H1='OData-Version: 4.0'
 
   if [ -n "${_redfish_use_basic_auth}" ]; then
+    _info "Authenticating with Redfish API on ${_redfish_host} (basic HTTP auth)."
     _redfish_access_token="$(printf '%s:%s' "${_redfish_username}" "${_redfish_password}" | _base64)"
     export _H2="Authorization: Basic ${_redfish_access_token}"
   else
+    _info "Authenticating with Redfish API on ${_redfish_host}."
+
     # Get sessions endpoint URI path
     _redfish_response="$(_get "https://${_redfish_host}/redfish/v1/")"
     _redfish_session_path="$(echo "${_redfish_response}" | jq -r '.Links.Sessions.["@odata.id"]')"
-    _debug2 _redfish_response "${_redfish_response}"
 
     # Create new session
     _redfish_body="$(jq -n '{"UserName":$user,"Password":$pass}' --arg user "${_redfish_username}" --arg pass "${_redfish_password}" | _normalizeJson)"
@@ -240,7 +252,7 @@ _redfish_authenticate() {
     _code="$(_egrep_o <"${HTTP_HEADER}" '^HTTP[^ ]* [0-9]+' | _tail_n 1 | tr -d '\r\n' | cut -d ' ' -f 2)"
 
     # Verify authentication succeeded
-    if [ "${_code}" != "201" ]; then
+    if [ "${_code}" != '201' ]; then
       _err "Redfish authentication failed (HTTP ${_code})"
       _err "Response: ${_redfish_response}"
       return 1
@@ -256,18 +268,18 @@ _redfish_end_session() {
   _redfish_host="$1"
   _redfish_session_path="${2:-}"
 
-  _info 'Ending active Redfish session'
+  _info "Ending active Redfish session on ${_redfish_host}."
 
   if [ -n "${_redfish_session_path}" ]; then
     _redfish_response="$(_post '' "https://${_redfish_host}${_redfish_session_path}" '' 'DELETE')"
     _code="$(_egrep_o <"${HTTP_HEADER}" '^HTTP[^ ]* [0-9]+' | _tail_n 1 | tr -d '\r\n' | cut -d ' ' -f 2)"
 
-    if [ "${_code}" != "204" ]; then
+    if [ "${_code}" != '204' ]; then
       _err "Failed to log out of Redfish server (HTTP ${_code})."
       _err "Response: ${_redfish_response}"
     fi
   else
-    _info 'Using basic HTTP auth, no need to sign out.'
+    _info 'Using basic HTTP auth, no need to sign out of Redfish API.'
   fi
 
   export _H1=
